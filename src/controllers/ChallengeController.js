b13c0a94168067d32692a017516044cd64b1ffec
Pedro Ramos <pedroramos.contato@outlook.com>
2020-06-28 16:01:20 -0300

    Delete challenge details controller:
    Moved `index` method in `ChallengeDetailController.js` to `show` method in `ChallengeController.js`.
    Since you're searching for a single challenge, there's no point in creating a new controller just for that.

--- src/controllers/ChallengeController.js
+++ src/controllers/ChallengeController.js
@@ -8,7 +8,18 @@ module.exports = {
     },
 
     async store(request, response){
-        const {type, name, description,level,techs,background,images, github_url,brief,dev_id} = request.body;
+        const {
+            type,
+            name,
+            description,
+            level,
+            techs,
+            background,
+            images,
+            github_url,
+            brief,
+            dev_id
+        } = request.body;
 
         const challenge = await Challenge.create({
             type,
@@ -22,6 +33,14 @@ module.exports = {
             brief,
             dev_id
         });
+
+        return response.json(challenge);
+    },
+
+    async show (request, response) {
+        const { challenge_id } = request.params;
+        
+        const challenge = await Challenge.find({ _id: challenge_id}).populate('dev_id');
 
         return response.json(challenge);
     }
--- src/routes.js
+++ src/routes.js
@@ -2,19 +2,16 @@ const express = require('express');
 
 const DevController = require('./controllers/DevController');
 const ChallengeController = require('./controllers/ChallengeController');
-const ChallengeDetailController = require('./controllers/ChallengeDetailController');
 const NewsletterController = require('./controllers/NewsletterController');
 
 const routes = express.Router();
 
-routes.get('/challenges/:challenge_id', ChallengeDetailController.index);
-
 routes.get('/challenges', ChallengeController.index);
+routes.get('/challenges/:challenge_id', ChallengeController.show);
 routes.post('/challenges', ChallengeController.store);
 
 routes.get('/devs', DevController.index);
 routes.post('/devs', DevController.store);
-
 
 routes.get('/newsletter', NewsletterController.index);
 routes.post('/newsletter', NewsletterController.store);
