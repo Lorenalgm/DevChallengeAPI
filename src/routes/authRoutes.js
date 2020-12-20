d104b9bab67c628f2cdbe3942fde69b0f1205866
stevescruz <steve.silverio.cruz@gmail.com>
2020-12-20 06:02:13 -0500

    Created Authentication Controller -Modified authRoutes.js to transfer code related to passport to the authentication controller.

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -1,37 +1,22 @@
 const authRouter = require('express').Router();
-const passport = require('passport');
+
+const AuthenticationController = require('../controllers/AuthenticationController');
 
 // auth login
-authRouter.get('/login', (request, response) => {
-  if (!request.user) {
-    response.send('login');
-  } else {
-    response.redirect('/profile');
-  }
-});
+authRouter.get('/login', AuthenticationController.login);
 
 // auth logout
-authRouter.get('/logout', (request, response) => {
-  request.logout();
-  response.redirect('/');
-});
+authRouter.get('/logout', AuthenticationController.logout);
 
 // auth with github
-authRouter.get(
-  '/github',
-  passport.authenticate('github', {
-    scope: ['read:user']
-  })
-);
+authRouter.get('/github', AuthenticationController.oAuthGitHubAuthorization());
 
 // callback route for github to redirect to
 
 authRouter.get(
   '/github/callback',
-  passport.authenticate('github'),
-  (request, response) => {
-    response.redirect('/profile');
-  }
+  AuthenticationController.oAuthGitHubRequestUserData(),
+  AuthenticationController.oAuthCallbackRedirect
 );
 
 module.exports = authRouter;
