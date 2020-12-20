5f5089de46cdc9ae5791e7fcc2ca6f0986b8b054
stevescruz <steve.silverio.cruz@gmail.com>
2020-12-20 01:15:43 -0500

    Used dotenv environment variables to define GitHub Strategy The GitHub passport strategy at passportSetup requires a callback URL, a client ID and a client secret.

--- src/config/passportSetup.js
+++ src/config/passportSetup.js
@@ -1,3 +1,9 @@
+require('dotenv').config({
+  path:
+    process.env.NODE_ENV === 'production'
+      ? '.env'
+      : `.env.${process.env.NODE_ENV}`
+});
 const passport = require('passport');
 const GitHubStrategy = require('passport-github2');
 
@@ -5,7 +11,12 @@ passport.use(
   new GitHubStrategy(
     {
       // options for the strategy
+      callbackURL: `${process.env.APP_URL}/auth/github/callback`,
+      clientID: process.env.GITHUB_CLIENT_ID,
+      clientSecret: process.env.GITHUB_CLIENT_SECRET
     },
-    () => {}
+    () => {
+      // passport callback function
+    }
   )
 );
