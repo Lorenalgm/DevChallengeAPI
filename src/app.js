ea340de1c5ff644251d6f8b753c5f1f4f7dfbcdb
stevescruz <steve.silverio.cruz@gmail.com>
2020-12-20 04:18:08 -0500

    Implemented cookie session -Modified app.js to implement a cookie session that is encrypted and decrypted by using a key

--- src/app.js
+++ src/app.js
@@ -7,6 +7,8 @@ require('dotenv').config({
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
+const cookieSession = require('cookie-session');
+const passport = require('passport');
 
 const errorHandler = require('./middlewares/errorHandler');
 const routeNotFound = require('./middlewares/routeNotFound');
@@ -24,6 +26,17 @@ mongoose.connect(process.env.MONGO_URL, {
 
 app.use(cors());
 app.use(express.json());
+
+app.use(
+  cookieSession({
+    maxAge: 24 * 60 * 60 * 1000,
+    keys: [process.env.PASSPORT_SESSION_COOKIE_KEY]
+  })
+);
+
+app.use(passport.initialize());
+app.use(passport.session());
+
 app.use(routes);
 
 app.use(routeNotFound);
