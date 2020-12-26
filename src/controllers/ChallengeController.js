2c733229a899bea59f5a8814c40411822f4ccaa8
BrunoBatalha <brunobatalha37@gmail.com>
2020-12-26 15:54:03 -0400

    Adiciona filtro em desafios por: level e techs

--- src/controllers/ChallengeController.js
+++ src/controllers/ChallengeController.js
@@ -2,14 +2,15 @@ const Challenge = require('../models/Challenge');
 
 module.exports = {
   async index(request, response) {
-    const { type } = request.query;
+    const { type, level, techs } = request.query;
+    const filter = {};
     let challenges = [];
 
-    if (type) {
-      challenges = await Challenge.find({ type });
-    } else {
-      challenges = await Challenge.find();
-    }
+    if (type) filter.type = type;
+    if (level) filter.level = level;
+    if (techs) filter.techs = techs;
+
+    challenges = await Challenge.find(filter);
 
     return response.json(challenges);
   },
