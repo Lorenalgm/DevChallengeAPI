14d5c1a4b4b3632ad2bf670d3310060e8fcef080
Pedro Ramos <pedro.henrique@codeminer42.com>
2021-02-01 09:43:18 -0300

    Refactor app boot logic
    Modularize the app initialization file in order to achieve conciseness and a single responsibility for each boot step

--- src/app.js
+++ src/app.js
@@ -1,39 +1,15 @@
 require('./config/dotenv');
+require('./providers/passport');
 
 const express = require('express');
-const cors = require('cors');
-const cookieSession = require('cookie-session');
-const passport = require('passport');
 
+const boot = require('./app/boot/express');
 const database = require('./database/init');
-
-const errorHandler = require('./middlewares/errorHandler');
-const routeNotFound = require('./middlewares/routeNotFound');
-
-require('./providers/passport/index');
-
-const routes = require('./routes');
 
 const app = express();
 
 database.init();
 
-app.use(cors());
-app.use(express.json());
-
-app.use(
-  cookieSession({
-    maxAge: 24 * 60 * 60 * 1000,
-    keys: [process.env.PASSPORT_SESSION_COOKIE_KEY]
-  })
-);
-
-app.use(passport.initialize());
-app.use(passport.session());
-
-app.use(routes);
-
-app.use(routeNotFound);
-app.use(errorHandler);
+boot(app);
 
 module.exports = app;
