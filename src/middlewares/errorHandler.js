6c3089a32c8c4671aefcc3698015ae463c5edad2
stevescruz <steve.silverio.cruz@gmail.com>
2021-02-12 18:35:17 -0500

    REF: errorHandler was refactored into a named function that handles multiple types of errors

--- src/middlewares/errorHandler.js
+++ src/middlewares/errorHandler.js
@@ -1,6 +1,16 @@
-// eslint-disable-next-line no-unused-vars
-module.exports = (error, _request, response, _next) => {
-  const { status, ...err } = error;
+const AppError = require('../shared/errors/AppError');
 
-  return response.status(status || 500).send(err);
-};
+// eslint-disable-next-line no-unused-vars
+function errorHandler(error, _request, response, _next) {
+  if (error instanceof AppError) {
+    return response
+      .status(error.statusCode)
+      .json({ status: 'error', message: error.message });
+  }
+
+  return response
+    .status(500)
+    .json({ status: 'error', message: 'Internal server error' });
+}
+
+module.exports = errorHandler;
