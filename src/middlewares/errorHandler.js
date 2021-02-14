d0d40631bd453d3e3d9ffb4f4ef5f1352a4a40a2
stevescruz <steve.silverio.cruz@gmail.com>
2021-02-13 20:12:35 -0500

    REF: error handler to handle Mongoose Validation Error

--- src/middlewares/errorHandler.js
+++ src/middlewares/errorHandler.js
@@ -1,16 +1,25 @@
+const { Error: MongooseError } = require('mongoose');
+
 const AppError = require('../shared/errors/AppError');
 
 // eslint-disable-next-line no-unused-vars
 function errorHandler(error, _request, response, _next) {
-  if (error instanceof AppError) {
-    return response
-      .status(error.statusCode)
-      .json({ status: 'error', message: error.message });
+  switch (error instanceof Error) {
+    case error instanceof AppError:
+      return response.status(error.statusCode).json({
+        status: 'error',
+        message: error.message
+      });
+    case error instanceof MongooseError.ValidationError:
+      return response
+        .status(400)
+        .json({ status: 'error', message: error.message });
+    default:
+      return response.status(500).json({
+        status: 'error',
+        message: 'Internal server error'
+      });
   }
-
-  return response
-    .status(500)
-    .json({ status: 'error', message: 'Internal server error' });
 }
 
 module.exports = errorHandler;
