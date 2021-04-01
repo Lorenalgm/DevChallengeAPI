d97ec43c622afe1041e768382372ce4a0dc8ecc2
Pedro Ramos <pedro.henrique@codeminer42.com>
2021-04-01 11:47:54 -0300

    Move schemas to infrastructure

---
+++ src/infrastructure/database/mongodb/schemas/Newsletter.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+
+const NewsletterSchema = new mongoose.Schema(
+  {
+    email: String
+  },
+  {
+    timestamps: true
+  }
+);
+
+module.exports = mongoose.model('Newsletter', NewsletterSchema);
