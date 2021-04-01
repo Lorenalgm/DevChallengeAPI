72f1290715358658e6795fb433ceeedc63116bcb
Pedro Ramos <pedro.henrique@codeminer42.com>
2021-04-01 11:52:54 -0300

    Delete config folder
    Remove the config folder to set this responsibility to the infrastructure layer

--- src/config/database.js
+++
@@ -1,6 +0,0 @@
-exports.MONGO_URL = process.env.MONGO_URL;
-
-exports.databaseOptions = {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-};
--- src/config/dotenv.js
+++
@@ -1,6 +0,0 @@
-require('dotenv').config({
-  path:
-    process.env.NODE_ENV === 'production'
-      ? '.env'
-      : `.env.${process.env.NODE_ENV}`
-});
