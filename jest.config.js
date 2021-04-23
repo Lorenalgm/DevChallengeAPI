a61e806f2cc0769d423b98d59733d277971dc091
Pedro Ramos <pedro.henrique@codeminer42.com>
2021-04-23 12:25:18 -0300

    Update bail config

--- jest.config.js
+++ jest.config.js
@@ -6,7 +6,7 @@ module.exports = {
   // automock: false,
 
   // Stop running tests after `n` failures
-  bail: true,
+  bail: false,
 
   // The directory where Jest should store its cached dependency information
   // cacheDirectory: "/tmp/jest_rs",
