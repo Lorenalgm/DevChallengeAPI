da1e79583ee74a2f03893130ce0e42d10131dc98
Pedro Ramos <pedro.henrique@codeminer42.com>
2021-04-23 15:14:17 -0300

    Add challenge repository tests

--- src/infrastructure/database/mongodb/repository/test/ChallengeRepository.test.js
+++ src/infrastructure/database/mongodb/repository/test/ChallengeRepository.test.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 
 const ChallengeRepository = require('../ChallengeRepository');
+const Challenge = require('../../../../../domain/Challenge');
 
 beforeAll(async () => {
   const url = process.env.MONGO_URL;
@@ -14,7 +15,24 @@ afterAll(async () => {
   await mongoose.connection.close();
 });
 
+afterEach(async () => {
+  await mongoose.connection.dropDatabase();
+});
+
 describe('Testing ChallengeRepository', () => {
+  const challengeMock = new Challenge({
+    type: 'Frontend',
+    name: 'placeholder',
+    description: 'placeholder',
+    level: 'placeholder',
+    techs: 'placeholder',
+    background: 'placeholder',
+    images: ['placeholder'],
+    github_url: 'placeholder',
+    brief: 'placeholder',
+    dev_id: mongoose.Types.ObjectId()
+  });
+
   describe('.created', () => {
     describe('when a invalid challenge object is passed', () => {
       it('throws an ValidationError', async () => {
@@ -25,9 +43,123 @@ describe('Testing ChallengeRepository', () => {
         }
       });
     });
+
+    describe('when a valid challenge object is passed', () => {
+      it('successfully persists a new challenge', async () => {
+        const result = await new ChallengeRepository().create(challengeMock);
+
+        expect(result).toEqual(
+          expect.objectContaining({
+            id: expect.any(String),
+            type: expect.any(String),
+            name: expect.any(String),
+            description: expect.any(String),
+            level: expect.any(String),
+            background: expect.any(String),
+            github_url: expect.any(String),
+            brief: expect.any(String),
+            techs: expect.any(Array),
+            images: expect.any(Array)
+          })
+        );
+      });
+    });
   });
 
-  describe('.fetchAll', () => {});
+  describe('.fetchAll', () => {
+    describe('when there are no registered challenges', () => {
+      it('returns an empty array', async () => {
+        const repository = new ChallengeRepository();
+        const challenges = await repository.fetchAll();
 
-  describe('.fetchById', () => {});
+        expect(challenges).toEqual([]);
+      });
+    });
+
+    describe('when no filter is provided', () => {
+      it('returns all registered challenges', async () => {
+        const repository = new ChallengeRepository();
+
+        await repository.create(challengeMock);
+        await repository.create(challengeMock);
+        await repository.create(challengeMock);
+
+        const challenges = await repository.fetchAll();
+
+        expect(challenges).toHaveLength(3);
+      });
+    });
+
+    describe('when a filter is provided', () => {
+      it('returns the challenges that match the filter', async () => {
+        const repository = new ChallengeRepository();
+
+        await repository.create(challengeMock);
+        await repository.create(challengeMock);
+        await repository.create({ ...challengeMock, type: 'Backend' });
+
+        const challenges = await repository.fetchAll({ type: 'Backend' });
+
+        expect(challenges).toHaveLength(1);
+      });
+    });
+  });
+
+  describe('.fetchById', () => {
+    describe('when no id is provided', () => {
+      it('returns null', async () => {
+        const challenge = await new ChallengeRepository().fetchById();
+
+        expect(challenge).toBeNull();
+      });
+    });
+
+    describe('when a invalid MongoDB id is provided', () => {
+      it('throws an CastError', async () => {
+        const repository = new ChallengeRepository();
+
+        try {
+          await repository.fetchById('invalid_id');
+        } catch (e) {
+          expect(e instanceof mongoose.Error.CastError).toBeTruthy();
+        }
+      });
+    });
+
+    describe('when a valid id is provided', () => {
+      describe('and the id does not corresponds to a challenge', () => {
+        it('returns null', async () => {
+          const id = mongoose.Types.ObjectId();
+
+          const challenge = await new ChallengeRepository().fetchById(id);
+
+          expect(challenge).toBeNull();
+        });
+      });
+
+      describe('and the id corresponds to a challenge', () => {
+        it('returns a single challenge object', async () => {
+          const repository = new ChallengeRepository();
+          const { _id: challengeId } = await repository.create(challengeMock);
+
+          const challenge = await repository.fetchById(challengeId);
+
+          expect(challenge).toEqual(
+            expect.objectContaining({
+              id: expect.any(String),
+              type: expect.any(String),
+              name: expect.any(String),
+              description: expect.any(String),
+              level: expect.any(String),
+              background: expect.any(String),
+              github_url: expect.any(String),
+              brief: expect.any(String),
+              techs: expect.any(Array),
+              images: expect.any(Array)
+            })
+          );
+        });
+      });
+    });
+  });
 });
